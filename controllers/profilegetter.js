feb94608e2cd2bd799576485b3502f111aed7c86
gold-olar <sam99kupo@gmail.com>
2019-06-17 03:02:20 +0100

    --

--- controllers/profilegetter.js
+++ controllers/profilegetter.js
@@ -1,33 +1,20 @@
 const User = require('../models/User');
 
-const profilegetter = (req, res, next)=>{
-    const {id} = req.params;
-    User.findOne({
-        _id: id
-    }).then(user =>{
-        if (user){
-            res.status(200).json({
-                message: 'User Found',
-                username: user.username,
-                email: user.email,
-                links: user.links
-            })
-        }else{
-            res.status(400).json({
-               message: 'User Not Found'
-            })
-            
-        }
+const profilegetter = (req, res, next) => {
+    const { id } = req.params;
+    User.findOneAndUpdate({ _id: id }, { $inc: { links: 1 } })
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(400).json({
+                    message: 'User Not Found'
+                })
 
-    })
-    .catch(err=>{
-        res.status(400).json({
-            error: err,
-            message: 'User Not found'
+            }
         })
-       
 
-    })
+
 
 }
 module.exports = profilegetter;
