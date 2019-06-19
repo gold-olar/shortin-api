89ddc04cecbfb460b914d74b4f03e085e1ad7509
gold-olar <sam99kupo@gmail.com>
2019-06-19 21:50:25 +0100

    '

--- controllers/addcontroller.js
+++ controllers/addcontroller.js
@@ -2,24 +2,30 @@ const Links = require('../models/Links');
 
 
 const addLink = (req, res) => {
-    let errors =[]
-    if(!req.body){
-        errors.push({'message': 'Please Enter a link to shortin'})
+    let errors = []
+    if (!req.body) {
+        errors.push({ 'message': 'Please Enter a link to shortin' })
     }
-    if (errors.length > 0){
+    if (errors.length > 0) {
         res.status(406).json(errors)
-    }else{
-        const {longlink, shortlink, username } = req.body
+    } else {
+        const { longlink, shortlink, username } = req.body
+        console.log(longlink, shortlink)
         const newLink = {
             long: longlink,
-            short: shortlink ,
+            short: shortlink,
             username: username
         }
         new Links(newLink)
             .save()
-            .then(links=> {
-                console.log(newLink)
-                res.status(200).json('Added to database');
+            .then(links => {
+                if (!links) {
+                    throw err;
+                } else {
+                    console.log(newLink);
+                    res.status(200).json('Added to database');
+                }
+
             });
     }
 };
