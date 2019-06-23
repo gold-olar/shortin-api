f52f1d2244a09f73c02661e77d10852135fba85b
gold-olar <sam99kupo@gmail.com>
2019-06-23 17:00:35 +0100

    im in sifia pains... i lost all what i worked on overnight.....

--- controllers/shortincontroller.js
+++ controllers/shortincontroller.js
@@ -1,59 +1,30 @@
 const validator = require('validator');
 const randomstring = require("randomstring");
 const Links = require('../models/Links');
+const User = require('../models/User');
 
 
 const shortin = (req, res) => {
     const { longlink, username } = req.body;
-    console.log(76897)
-    try{
-            if (validator.isURL(longlink)) {
-        //Generate Random String
-        const generated_string = randomstring.generate({
-            length: 7,
-            charset: 'alphabetic'
-        });
-        //Search database if link has already been shortined prev
-        Links.findOne({
-            long: longlink
-        })
-            .then(link => {
-                //If link hasn't been previously shortined, Save newlink
-                if (!link) {
-                    const newLink = {
-                        long: longlink,
-                        short: `${req.headers.host}/${generated_string}`,
-                        username: username,
-                        linkfinder: generated_string
-                    }
-                    new Links(newLink)
-                        .save()
-                        .then(links => {
-                            res.status(200).json({
-                                'Message': 'Shortined',
-                                'shortined_link': newLink.short,
-                                'longlink': newLink.long
-                            })
+    //console.log(76897)
+    try {
+        User.findOne({ username: username })
+            .then(user => {
+                if (user) {
 
-                        });
                 } else {
-                    res.status(409).json({
-                        'message': 'Link has previously been Shortined.!!!',
-                        'shortined_as': link.short
+                    res.status(400).json({
+                        failure_message: 'Please create an account before you can use our services...'
                     })
                 }
             })
-    } else {
-        res.status(400).json(`Url ${longlink} is not a valid url`);
-    };
-
     }
-    catch(err){
-        if(err){
-            console.log(err)
+    catch (err) {
+            if (err) {
+                console.log(err)
+            }
+
         }
 
     }
-
-}
 module.exports = shortin;
